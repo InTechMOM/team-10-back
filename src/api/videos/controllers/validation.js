88507d421e0436a57ad0b27da71178043cc62a26
Carolina <angiedylexx@gmail.com>
2023-05-02 17:28:57 -0500

    S e agregó validación de Joi para la url del video

--- src/api/videos/controllers/validation.js
+++ src/api/videos/controllers/validation.js
@@ -3,11 +3,11 @@ import Joi from "joi";
 //Esquema de carga de Video
 export const SchemaUpload =   Joi.object ({
     email: Joi.string().required().min(8).max(32).email({minDomainSegments:2, tlds:{allow:["com","net"]}}),
-    url: Joi.string().required()
+    url: Joi.string().required().uri().regex(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i)
 })
 
 // Esquema Modificación (se dejan opcionales)
 export const SchemaUpdate =   Joi.object ({
     email: Joi.string().min(8).max(32).email({minDomainSegments:2, tlds:{allow:["com","net"]}}),
-    url: Joi.string().required()
+    url: Joi.string().required().uri().regex(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i)
 })

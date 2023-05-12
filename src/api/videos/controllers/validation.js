0fc451bf0e3035ea89df5497527db92983dcfd6a
Carolina <angiedylexx@gmail.com>
2023-05-11 22:05:20 -0500

    se dejo usqueda de ususrio por id, se agrego el apellido al login, se camia el name por el firstname

--- src/api/videos/controllers/validation.js
+++ src/api/videos/controllers/validation.js
@@ -8,6 +8,5 @@ export const SchemaUpload =   Joi.object ({
 
 // Esquema Modificación (se dejan opcionales)
 export const SchemaUpdate =   Joi.object ({
-    email: Joi.string().min(8).max(32).email({minDomainSegments:2, tlds:{allow:["com","net"]}}),
     url: Joi.string().required().uri().regex(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i)
 })
